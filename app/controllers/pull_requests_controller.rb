e7f1bbc69809fcd569ec3a8c3c3a855032103fc7
Patrick McKenna <patrickmckenna@github.com>
2015-10-13 07:47:12 -0700

    fix controller, view to match simplified schema

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,9 +1,7 @@
 class PullRequestsController < ApplicationController
 
   def show
-    @pull_request = PullRequest.new owner: "patrickmckenna", repo: "scratch",
-                                    sha: "993e3925117198c374182df6aea707e97e9692ab",
-                                    status: "pending"
+    @pull_request = PullRequest.new "https://api.github.com/repos/patrickmckenna/scratch/stauses/993e3925117198c374182df6aea707e97e9692ab"
   end
 
   def create
