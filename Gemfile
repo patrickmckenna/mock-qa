761cbe32d2281ea0a7f1f6a22df7c768c7ceefa2
Patrick McKenna <patrickmckenna@github.com>
2015-10-15 14:45:18 -0700

    WIP works but needs cleanup

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'mini_magick',              '3.8.0'
 gem 'fog',                      '1.26.0'
 gem 'will_paginate',            '3.0.7'
 gem 'bootstrap-will_paginate',  '0.0.10'
+gem 'octokit'
 
 
 
--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,3 +1,5 @@
+require 'octokit'
+
 class PullRequestsController < ApplicationController
 
   skip_before_filter :verify_authenticity_token, :only => [:create_update]
@@ -9,13 +11,23 @@ class PullRequestsController < ApplicationController
   def create_update
     url = params[:pull_request][:url]
     statuses_url = params[:pull_request][:statuses_url]
+    body = params[:pull_request][:repo][:full_name]
+    sha = params[:pull_request][:head][:sha]
     @pull_request = PullRequest.find_by(url: url)
     if @pull_request
       @pull_request.statuses_url = statuses_url
     else
-      @pull_request = PullRequest.new url: url, statuses_url: statuses_url
+      @pull_request = PullRequest.new url: url, statuses_url: sha
     end
     @pull_request.save
+
+    status = { target_url: 'https://whispering-retreat-8216.herokuapp.com/pull-requests',
+               description: 'an example description',
+               context: 'merge-manager' }
+    client = Octokit::Client.new access_token: 'acf7cbfb357975289ca9212aab0a5caef1933604'
+    client.create_status repo, sha, 'pending', status
+
+    redirect_to root_url
   end
 
 end
