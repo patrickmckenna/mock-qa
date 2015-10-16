ecddeec2b80db0f936ba1629382566bf3ff1125c
Patrick McKenna <patrickmckenna@github.com>
2015-10-16 13:53:28 -0700

    delete closed PRs, whether merged or not

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -14,21 +14,28 @@ class PullRequestsController < ApplicationController
     repo = 'patrickmckenna/scratch'
     sha = params[:pull_request][:head][:sha]
 
-  @pull_request = PullRequest.find_by(url: url)
-    if @pull_request
-      @pull_request.sha = sha
-    else
-      @pull_request = PullRequest.new url: url, repo: repo, sha: sha
+    pull_request_action = request.request_parameters[:action]
+    case pull_request_action
+    when 'opened', 'reopened'
+      PullRequest.create url: url, repo: repo, sha: sha
+    when 'synchronize'
+      PullRequest.find_by(url: url).update(sha: sha)
+    when 'closed'
+      PullRequest.find_by(url: url).destroy
     end
-    @pull_request.save
 
+    set_pending_state repo, sha #unless pull_request_action == 'closed'?
+    redirect_to root_url
+  end
+
+  private
+
+  def set_pending_state repo, sha
     sample_status = { target_url: 'https://merge-manager.herokuapp.com/pull-requests',
                       description: 'an example description',
                       context: 'merge-manager' }
     client = Octokit::Client.new access_token: ENV['GITHUB_ACCESS_TOKEN']
     client.create_status repo, sha, 'pending', sample_status
-
-    redirect_to root_url
   end
 
 end
